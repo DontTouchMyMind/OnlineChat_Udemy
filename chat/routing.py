3b70fcb779c9c95d76d44f0aaea04dd3f29d0964
DontTouchMyMind <tobigface@gmail.com>
2020-12-01 15:47:41 +0200

    Fixed routs.

--- chat/routing.py
+++ chat/routing.py
@@ -1,15 +1,9 @@
 from django.conf.urls import url
 
-from .consumers import ChatConsumer, AsyncChatConsumer, BaseSyncConsumer, AsyncBaseSyncConsumer, ChatJsonConsumer, ChatAsyncJsonConsumer, MyChatConsumer, MyAsyncChatConsumer
+from .consumers import ChatConsumer, GroupChatConsumer
 
 
 websocket_urls = [
-    url(r'^ws/chat/$', ChatConsumer.as_asgi()),
-    url(r'^ws/async_chat/$', AsyncChatConsumer.as_asgi()),
-    url(r'^ws/base_sync_chat/$', BaseSyncConsumer.as_asgi()),
-    url(r'^ws/base_async_chat/$', AsyncBaseSyncConsumer.as_asgi()),
-    url(r'^ws/json_chat/$', ChatJsonConsumer.as_asgi()),
-    url(r'^ws/async_json_chat/$', ChatAsyncJsonConsumer.as_asgi()),
-    url(r'^ws/chat/(?P<room_name>\w+)/$', MyChatConsumer.as_asgi()),
-    url(r'^ws/async_chat/(?P<room_name>\w+)/$', MyAsyncChatConsumer.as_asgi()),
+    url(r'^ws/groups/$', GroupChatConsumer.as_asgi()),
+    url(r'^ws/chat/(?P<group_id>\d+)/$', ChatConsumer.as_asgi())
 ]
